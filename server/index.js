eab2e16dc5851dd7c00aa9318764ed7b9d738b88
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-07-10 16:41:09 -0400

    Initial endpoint/heroku test

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const express = require('express');
 const app = express();
 
 // API endpoints go here!
-
+app.get('/api/hello', (req, res) => {
+  return rex.json('Hello, World');
+});
 
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -12,34 +14,34 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 // Unhandled requests which aren't for the API should serve index.html so
 // client-side routing using browserHistory can function
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
-    const index = path.resolve(__dirname, '../client/build', 'index.html');
-    res.sendFile(index);
+  const index = path.resolve(__dirname, '../client/build', 'index.html');
+  res.sendFile(index);
 });
 
 let server;
 function runServer(port=3001) {
-    return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            resolve();
-        }).on('error', reject);
-    });
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      resolve();
+    }).on('error', reject);
+  });
 }
 
 function closeServer() {
-    return new Promise((resolve, reject) => {
-        server.close(err => {
-            if (err) {
-                return reject(err);
-            }
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    server.close(err => {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
     });
+  });
 }
 
 if (require.main === module) {
-    runServer();
+  runServer();
 }
 
 module.exports = {
-    app, runServer, closeServer
+  app, runServer, closeServer
 };
