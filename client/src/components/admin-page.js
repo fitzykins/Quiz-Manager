7ff730dadd5eaf11c8bc67addb5818d85389514a
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-07-12 12:25:29 -0400

    Grab users and display on screen

--- client/src/components/admin-page.js
+++ client/src/components/admin-page.js
@@ -1,23 +1,42 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {getUser} from '../actions';
+import {fetchUser, fetchUsers} from '../actions';
+import {Link} from 'react-router-dom';
 
 
 
 class AdminPage extends Component {
   componentDidMount() {
-    this.props.dispatch(getUser(this.props.match.params.adminId));
+    this.props.dispatch(fetchUser(this.props.match.params.adminId));
+    this.props.dispatch(fetchUsers());
   }
 
   render () {
-      return (
-        <h3>{this.props.admin}</h3>
-      )
-    }
+   console.log(this.props);
+   const users = this.props.users.map((user, index) => {
+     return (
+       <li key={index}>
+         {user.userName}
+       </li>
+     )
+   })
+   return (
+    <div className='admin-view'>
+      <h3>{this.props.adminName}</h3>
+      <ul className="admin-users">
+        {users}
+      </ul>
+    </div>
+   )
   }
+}
 
 const mapStateToProps= state =>({
-  admin: state.username
+  adminId: state.userId,
+  adminName: state.userName,
+  users: state.users.filter(user => {
+    return !user.userName.toLowerCase().includes('admin');
+  })
 });
 
 export default connect(mapStateToProps)(AdminPage);
