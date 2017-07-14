f167c8e1e8a6a3f43f211d0d0ef4b1b8ecaf5999
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-07-14 13:38:29 -0400

    Change url to heroku app

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -87,7 +87,7 @@ export const fetchUpdateQuizSuccess = (quizName, score, status) => ({
 });
 
 export const fetchLogIn = (username, password) => dispatch => {
-  const url = 'http://localhost:8080/api/login';
+  const url = 'https://guiz-manager.herokuapp.com/api/login';
   dispatch(fetchRequest());
   return fetch(url, {
     method: 'get',
@@ -112,7 +112,7 @@ export const fetchUsers = () => (dispatch, getState) => {
   const state = getState();
   const username = state.loginName;
   const password = state.loginPass;
-  const url = 'http://localhost:8080/api/users';
+  const url = 'https://guiz-manager.herokuapp.com/api/users';
 
   dispatch(fetchRequest());
 
@@ -138,7 +138,7 @@ export const fetchUser = userId => (dispatch, getState) => {
   const state = getState();
   const username = state.loginName;
   const password = state.loginPass;
-  const url = `http://localhost:8080/api/users/${userId}`;
+  const url = `https://guiz-manager.herokuapp.com/api/users/${userId}`;
 
   dispatch(fetchRequest());
 
@@ -164,7 +164,7 @@ export const fetchQuiz = quizId => (dispatch, getState) => {
   const state = getState();
   const username = state.loginName;
   const password = state.loginPass;
-  const url = `http://localhost:8080/api/quizzes/${quizId}`;
+  const url = `https://guiz-manager.herokuapp.com/api/quizzes/${quizId}`;
 
   dispatch(fetchRequest());
 
@@ -190,7 +190,7 @@ export const updateQuiz = (quizName, userId, score, status) => (dispatch, getSta
   const state = getState();
   const username = state.loginName;
   const password = state.loginPass;
-  const url = `http://localhost:8080/api/updateuserquiz/${quizName}/${userId}`;
+  const url = `https://guiz-manager.herokuapp.com/api/updateuserquiz/${quizName}/${userId}`;
 
   dispatch(fetchRequest());
 
